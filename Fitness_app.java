ac3911b0391f8c30f39036ec35418d63ef3eb2f5
xtian_eugene <wanzalaeugenefredrick@gmail.com>
2025-11-11 19:47:40 +0300

    Update Fitness_app.java

--- Fitness_app.java
+++ Fitness_app.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 public class Fitness_app {
-    public static String getFitnessLevel(
+    public static int getFitnessLevel(
             int steps
     ){
 if(steps >= 10000){
-    return "Excellent";
+    System.out.println("Excellent");
 }
-else if (steps >= 7000 || steps < 10000) {
-    return "Good";
+else if (steps >= 7000) {
+    System.out.println("Good");
 }
-else if (steps >= 4000 || steps < 7000) {
-    return "Average";
+else if (steps >= 4000) {
+    System.out.println("Average");
 }
 else {
-    return "Poor";
+    System.out.println("Poor");
 }
+ return steps;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -22,7 +23,7 @@ else {
         System.out.print("Enter your steps for today: ");
         int steps = sc.nextInt();
 
-        String totalsteps = getFitnessLevel(steps);
+        int totalsteps = getFitnessLevel(steps);
         System.out.println("Your fitness level: "+ totalsteps);
 
         sc.close();
